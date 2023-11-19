f3b0ec92eaac5dcda811bfd07b39ead3d70c6fbf
theokoppler <theo.koppler@icloud.com>
2023-11-19 18:18:57 +0100

    Rebuild the ConfigScreen

--- src/client/java/net/The2019/NewBase/screen/ConfigScreen.java
+++ src/client/java/net/The2019/NewBase/screen/ConfigScreen.java
@@ -8,17 +8,12 @@ import net.minecraft.client.gui.widget.TextWidget;
 import net.minecraft.client.option.GameOptions;
 import net.minecraft.text.Text;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static net.The2019.NewBase.features.config.ModuleStates.*;
-
 public class ConfigScreen extends Screen {
     private final Screen parent;
     private final GameOptions settings;
     private static final MinecraftClient mc = MinecraftClient.getInstance();
-    private static List<ButtonWidget> buttonWidgetList = new ArrayList<>();
-    private int y = 30;
+    private int y = 50;
+    private int x = 20;
 
     public ConfigScreen(Screen parent, GameOptions gameOptions) {
         super(Text.translatable("newbase.configscreen.name"));
@@ -28,37 +23,9 @@ public class ConfigScreen extends Screen {
 
     @Override
     protected void init() {
-        buttonWidgetList.add((new ButtonWidget.Builder(Text.translatable("newbase.configscreen.togglecoordinates"), button -> {
-            coordinatesDisplayState = !coordinatesDisplayState;
-        }).dimensions(20, y, 150, 20).build()));
+        this.addDrawable(new TextWidget(x, 20, 100, 20, Text.translatable("newbase.configscreen.name"), textRenderer));
 
-        buttonWidgetList.add((new ButtonWidget.Builder(Text.translatable("newbase.configscreen.togglebiom"), button -> {
-            biomDisplayState = !biomDisplayState;
-        }).dimensions(20, y * 2, 150, 20).build()));
-
-        buttonWidgetList.add((new ButtonWidget.Builder(Text.translatable("newbase.configscreen.togglefps"), button -> {
-            fpsDisplayState = !fpsDisplayState;
-        }).dimensions(20, y * 3, 150, 20).build()));
-
-        this.addDrawable(new TextWidget(20, 10, 80, 10, Text.translatable("newbase.configscreen.name"), textRenderer));
-    }
-
-    @Override
-    public void tick() {
-        for (ButtonWidget buttonWidget : buttonWidgetList) {
-            boolean buttonState = false;
-
-            // Check which button is being processed
-            if (buttonWidget.getMessage().getString().equals("newbase.configscreen.togglecoordinates")) {
-                buttonState = coordinatesDisplayState;
-            } else if (buttonWidget.getMessage().getString().equals("newbase.configscreen.togglebiom")) {
-                buttonState = biomDisplayState;
-            } else if (buttonWidget.getMessage().getString().equals("newbase.configscreen.togglefps")) {
-                buttonState = fpsDisplayState;
-            }
-
-            buttonWidget.setTooltip(Tooltip.of(Text.of(isEnabled(buttonState))));
-            this.addDrawableChild(buttonWidget);
-        }
+        this.addDrawable(new TextWidget(x, y, 100, 20, Text.translatable("newbase.configscreen.hud"), mc.textRenderer));
+        this.addDrawableChild(new ButtonWidget.Builder(Text.translatable("newbase.configscreen.hudbutton"), button -> {mc.player.sendMessage(Text.literal("test"));}).tooltip(Tooltip.of(Text.translatable("newbase.configscreen.hudtooltip"))).dimensions(this.width - 220, y, 200, 20).build());
     }
 }
